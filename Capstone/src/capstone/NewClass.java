9609a14408b3fe722b7ebed9de91929697bf7411
Kairo <kairo.dianaphan@gmail.com>
2016-10-11 22:41:44 -0700

    Third commit!

---
+++ Capstone/src/capstone/NewClass.java
@@ -0,0 +1,19 @@
+package capstone;
+
+/**
+ *
+ * @author Kairo Phan
+ */
+public class NewClass {
+
+    private void MyFunction() {
+        /*
+         * 
+         * 
+         * And I'm also adding this new file!
+         * 
+         * 
+         * 
+         */
+    }
+}
